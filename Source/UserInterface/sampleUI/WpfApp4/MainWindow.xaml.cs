f5ff57a2d13ab065408f071677e2f00d3f6f091d
yskartal <githubgithub32>
2017-06-15 16:42:57 +0300

    user interface folder added

---
+++ Source/UserInterface/sampleUI/WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// MainWindow.xaml etkileşim mantığı
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            XmlDocument xmldoc;
+            InitializeComponent();
+        }
+        private void BrowseXmlFile(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.CheckFileExists = true;
+            dlg.Filter = "XML Files (*.xml)|*.xml|All Files(*.*)|*.*";
+            dlg.Multiselect = false;
+
+            if (dlg.ShowDialog() != true) { return; }
+
+            XmlDocument XMLdoc = new XmlDocument();
+            try
+            {
+                XMLdoc.Load(dlg.FileName);
+            }
+            catch (XmlException)
+            {
+                MessageBox.Show("The XML file is invalid");
+                return;
+            }
+            //freeArea.Text = XMLdoc.GetElementsByTagName("soa:AB_ID")[0].Value;
+            //txtFilePath.Text = dlg.FileName;
+            //vXMLViwer.xmlDocument = XMLdoc;
+            //xmldata = XMLdoc;
+            XMLdoc.Save("test-doc.xml");
+        }
+    }
+}
---
+++ Source/UserInterface/sampleUI/XMLViewer/Viewer.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows.Controls;
+using System.Windows.Data;
+using System.Xml;
+
+namespace XMLViewer
+{
+    /// <summary>
+    /// Interaction logic for Viewer.xaml
+    /// </summary>
+    public partial class Viewer : UserControl
+    {
+        private XmlDocument _xmldocument;
+        public Viewer()
+        {
+            InitializeComponent();
+        }
+
+        public XmlDocument xmlDocument
+        {
+            get { return _xmldocument; }
+            set
+            {
+                _xmldocument = value;
+                BindXMLDocument();
+            }
+        }
+
+        private void BindXMLDocument()
+        {
+            if (_xmldocument == null)
+            {
+                xmlTree.ItemsSource = null;
+                return;
+            }
+
+            XmlDataProvider provider = new XmlDataProvider();
+            provider.Document = _xmldocument;
+            Binding binding = new Binding();
+            binding.Source = provider;
+            binding.XPath = "child::node()";
+            xmlTree.SetBinding(TreeView.ItemsSourceProperty, binding);
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlNode rootNode = _xmldocument.CreateElement("DENEME");
+            _xmldocument.GetElementsByTagName("soa:AB_ID")[0].InnerText="0102030405";
+            //XmlNodeList nodelist = _xmldocument.SelectNodes("/soa:SOADataMaster/soa:AB_ID");
+            //nodelist[0].InnerText = "joojop";
+            //_xmldocument.SelectSingleNode("/soa:SOADataMaster/soa:AB_ID").InnerText = "NewValue";
+            //_xmldocument.AppendChild(rootNode);
+            _xmldocument.Save("test-doc.xml");
+        }
+    }
+}
